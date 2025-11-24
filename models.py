d3bc265aca01882809b676a97201c81d4a0b45d1
wilsonreimer <wilson.reimer@gmail.com>
2025-11-23 21:50:34 -0800

    Create models.py

---
+++ models.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, Integer, String, Float
+from .database import Base
+
+class Product(Base):
+    __tablename__ = "products"
+    id = Column(Integer, primary_key=True, index=True)
+    sku = Column(String, unique=True, index=True, nullable=False)
+    name = Column(String, nullable=False)
+    price = Column(Float)
+    quantity = Column(Integer)
